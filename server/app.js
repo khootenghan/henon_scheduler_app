538fb54d28cc1d21aa84b67a0777d44068656c68
Han Khoo <khootenghan7@gmail.com>
2025-04-27 04:09:29 -0400

    fix: adjust app get url matching for express v5

--- server/app.js
+++ server/app.js
@@ -15,12 +15,9 @@ app.use('/api/events', eventRoutes);
 
 if (process.env.NODE_ENV === 'production') {
   const buildPath = path.resolve(__dirname, '../client/build');
-  console.log('Serving static files from:', buildPath);
-
   app.use(express.static(buildPath));
-
-  console.log('serving next files from:', path.join(buildPath, 'index.html'));
-  app.get('*', (req, res) => {
+  
+  app.get('/{*any}', (req, res) => {
     res.sendFile(path.join(buildPath, 'index.html'));
   });
 } else {
@@ -28,9 +25,6 @@ if (process.env.NODE_ENV === 'production') {
     res.send('API is running (development mode)');
   });
 }
-
-console.log('NODE_ENV:', process.env.NODE_ENV);
-console.log('Static serving from:', path.join(__dirname, '../client/build'));
 
 // Sync DB and start server
 sequelize.sync().then(() => {
